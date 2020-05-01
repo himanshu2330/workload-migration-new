740871a666f42d1ada0d0e06dd34aed94eb42e27
himanshu2330 <him.shekhar89@gmail.com>
2020-05-01 10:14:47 -0700

    updated code

--- Scripts/Email.ps1
+++ Scripts/Email.ps1
@@ -7,11 +7,11 @@ $Result = Get-Content "${reportDirectory}/result.txt"
 $paramsLists = Get-Content "${workingDirectory}/Parameter/parameters.json" | ConvertFrom-Json
 
 $emailTo = $paramsLists.Request.RequesterEmailID
-$emailCC = "dinesh.ginwali@emc.com"
-$emailFrom = "dinesh.ginwali@emc.com"
+$emailCC = "dinesh.ginwali@xyz.com"
+$emailFrom = "dinesh.ginwali@xyz.com"
 $emailSubject = "VM Workload Migration : VM : $($VMInfo.Name) Cluster: $($VMInfo.VMHost.Parent )"
 $emailBody = [string]$Result 
-$smtpServer = "mailhub.lss.emc.com"
+$smtpServer = "xx.xx.xx.xx"
 $emailAttachments = "${reportDirectory}/$($paramsLists.Dest.Datacenter)/$($paramsLists.Dest.Environment)/$($paramsLists.Dest.HeadroomFileName)"
 
 
